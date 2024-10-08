bd774f957c2b5c610e92e401e77b1c810e5b0326
bumhee Cho <68563167+ChoBeomHee@users.noreply.github.com>
2024-10-08 23:11:10 +0900

    Title: 코드트리 투어, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 241008/코드트리 투어/codetree-tour.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <queue>
+#define MAX 2001
+
+using namespace std;
+int n, m;
+vector<pair<int, int>> graph[MAX];
+priority_queue<pair<pair<int, int>, int>> contents;
+int dist[MAX];
+int destination[MAX];
+bool isDelete[MAX];
+
+int start = 0;
+
+void dijk()
+{
+    priority_queue<pair<int, int>> pq;
+
+    for (int i = 0; i <= n; i++)
+    {
+        dist[i] = 10101010;
+    }
+    pq.push({0, start});
+    dist[start] = 0;
+
+    while (!pq.empty())
+    {
+        int nowNode = pq.top().second;
+        pq.pop();
+        for (int i = 0; i < graph[nowNode].size(); i++)
+        {
+            int nextNode = graph[nowNode][i].first;
+            int cost = graph[nowNode][i].second;
+
+            if (dist[nextNode] > dist[nowNode] + cost)
+            {
+                dist[nextNode] = dist[nowNode] + cost;
+                pq.push({-cost, nextNode});
+            }
+        }
+    }
+}
+
+int main()
+{
+    int Q;
+    cin >> Q;
+
+    for (int i = 0; i < Q; i++)
+    {
+        int commend;
+        cin >> commend;
+
+        if (commend == 100)
+        {
+            cin >> n >> m;
+            for (int i = 0; i < m; i++)
+            {
+                int a, b, c;
+                cin >> a >> b >> c;
+
+                graph[a].push_back({b, c});
+                graph[b].push_back({a, c});
+            }
+
+            dijk();
+        }
+        else if (commend == 200)
+        {
+            int id, rev, dest;
+            cin >> id >> rev >> dest;
+
+            int value = rev - dist[dest];
+            contents.push({{value, -id}, rev});
+            destination[id] = dest;
+        }
+        else if (commend == 300)
+        {
+            int idx;
+            cin >> idx;
+
+            isDelete[idx] = true;
+        }
+        else if (commend == 400)
+        {
+            bool flag = false;
+
+            priority_queue<pair<pair<int, int>, int>> tmp_pq = contents;
+
+            while (!tmp_pq.empty())
+            {
+                int cost = tmp_pq.top().first.first;
+                int idx = -tmp_pq.top().first.second;
+                tmp_pq.pop();
+
+                if (isDelete[idx] || cost < 0)
+                    continue;
+                flag = true;
+                cout << idx << '\n';
+                break;
+            }
+
+            if (!flag)
+                cout << -1 << '\n';
+            else
+                contents = tmp_pq;
+        }
+        else if (commend == 500)
+        {
+            int newStart;
+            cin >> newStart;
+
+            start = newStart;
+
+            dijk();
+
+            // for(int i = 0; i <= n; i++) cout << dist[i] << ' ';
+            // cout << endl;
+
+            priority_queue<pair<pair<int, int>, int>> tmp_pq = contents;
+
+            while (!contents.empty())
+            {
+                int idx = contents.top().first.second;
+                int value = contents.top().second - dist[destination[-idx]];
+                int origin = contents.top().second;
+                contents.pop();
+                if (!isDelete[-idx]){
+                    tmp_pq.push({{value, idx}, origin});
+                    //cout << "새로운 값 : " << -idx << ' ' << value << endl;
+                }
+            }
+
+            contents = tmp_pq;
+        }
+    }
+}
